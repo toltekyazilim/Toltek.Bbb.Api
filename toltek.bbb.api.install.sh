19caa019bfa4bc4319f7547082e787bc7e3c1f4e
Toltek <65180060+toltekdev@users.noreply.github.com>
2020-05-20 00:36:29 +0300

    path issue test

--- toltek.bbb.api.install.sh
+++ toltek.bbb.api.install.sh
@@ -1,11 +1,11 @@
-﻿# Toltek Bigbluebutton Api - Install Bash
+# Toltek Bigbluebutton Api - Install Bash
 # Yavuz 19/05/2020
 echo Toltek.Bbb.Api
 
 echo Toltek install dotnet
 sudo su
 
-wget -qO - https://packages.microsoft.com/config/ubuntu/19.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo wget -qO - https://packages.microsoft.com/config/ubuntu/19.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 
 yes | sudo apt-get update
@@ -19,27 +19,27 @@ yes | sudo apt-get install dotnet-sdk-3.1
 echo Toltel install Api
 
 
-sudo mkdir /home/toltek
+mkdir /home/toltek
 cd  /home/toltek/
-sudo git clone https://github.com/toltekyazilim/Toltek.Bbb.Api.git
+sudo git clone https://github.com/toltekyazilim/Toltek.Bbb.Api.git /home/toltek/Toltek.Bbb.Api
 cd  /home/toltek/Toltek.Bbb.Api
-sudo git pull
-sudo dotnet dev-certs https --trust
+git pull
+dotnet dev-certs https --trust
 
 echo Toltek configure nginx
 
-sudo rm /etc/bigbluebutton/nginx/toltek.bbb.api.nginx
-sudo ln -s /home/toltek/Toltek.Bbb.Api/toltek.bbb.api.nginx /etc/bigbluebutton/nginx/toltek.bbb.api.nginx
-sudo service nginx reload
+rm /etc/bigbluebutton/nginx/toltek.bbb.api.nginx
+ln -s /home/toltek/Toltek.Bbb.Api/toltek.bbb.api.nginx /etc/bigbluebutton/nginx/toltek.bbb.api.nginx
+service nginx reload
 
-sudo echo Toltek configure service
+echo Toltek configure service
 
-sudo systemctl stop toltek.bbb.api.service
-sudo systemctl disable toltek.bbb.api.service
+systemctl stop toltek.bbb.api.service
+systemctl disable toltek.bbb.api.service
 
-sudo rm /etc/systemd/system/toltek.bbb.api.service
-sudo ln -s /home/toltek/Toltek.Bbb.Api/toltek.bbb.api.service /etc/systemd/system/toltek.bbb.api.service
+rm /etc/systemd/system/toltek.bbb.api.service
+ln -s /home/toltek/Toltek.Bbb.Api/toltek.bbb.api.service /etc/systemd/system/toltek.bbb.api.service
 
-sudo systemctl enable toltek.bbb.api.service
-sudo systemctl start toltek.bbb.api.service
-sudo systemctl status toltek.bbb.api.service
+systemctl enable toltek.bbb.api.service
+systemctl start toltek.bbb.api.service
+systemctl status toltek.bbb.api.service
