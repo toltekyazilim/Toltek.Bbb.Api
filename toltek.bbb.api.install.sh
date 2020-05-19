4b3c0553645e9d24289ef5bbb9253cad03d3ba22
Yavuz Coşar <yavuzcosar@gmail.com>
2020-05-20 00:02:18 +0300

    update bash

--- toltek.bbb.api.install.sh
+++ toltek.bbb.api.install.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
-# Toltek Bigbluebutton Api - Install Bash
+﻿# Toltek Bigbluebutton Api - Install Bash
 # Yavuz 19/05/2020
-echo Toltek.Bbb.Api install started
+echo Toltek.Bbb.Api
 
-
+echo Toltek install dotnet
 
 wget -qO - https://packages.microsoft.com/config/ubuntu/19.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
@@ -15,6 +14,10 @@ yes | sudo apt-get update
 yes | sudo apt-get install aspnetcore-runtime-3.1
 yes | sudo apt-get install dotnet-sdk-3.1
 
+
+echo Toltel install Api
+
+
 mkdir /home/toltek
 cd  /home/toltek/
 git clone https://github.com/toltekyazilim/Toltek.Bbb.Api.git
@@ -22,10 +25,20 @@ cd  /home/toltek/Toltek.Bbb.Api
 git pull
 dotnet dev-certs https --trust
 
+echo Toltek configure nginx
+
+rm /etc/bigbluebutton/nginx/toltek.bbb.api.nginx
 ln -s /home/toltek/Toltek.Bbb.Api/toltek.bbb.api.nginx /etc/bigbluebutton/nginx/toltek.bbb.api.nginx
 service nginx reload
 
+echo Toltek configure service
+
+sudo systemctl stop toltek.bbb.api.service
+sudo systemctl disable toltek.bbb.api.service
+
+rm /etc/systemd/system/toltek.bbb.api.service
 ln -s /home/toltek/Toltek.Bbb.Api/toltek.bbb.api.service /etc/systemd/system/toltek.bbb.api.service
+
 sudo systemctl enable toltek.bbb.api.service
 sudo systemctl start toltek.bbb.api.service
 sudo systemctl status toltek.bbb.api.service
