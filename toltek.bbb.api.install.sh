64aa9633289ea876fc2d9de6df97b54079ae4198
Toltek <65180060+toltekdev@users.noreply.github.com>
2020-05-19 23:52:02 +0300

    api service enable

--- toltek.bbb.api.install.sh
+++ toltek.bbb.api.install.sh
@@ -1,4 +1,4 @@
-﻿#!/bin/bash
+#!/bin/bash
 # Toltek Bigbluebutton Api - Install Bash
 # Yavuz 19/05/2020
 echo Toltek.Bbb.Api install started
@@ -26,5 +26,6 @@ ln -s /home/toltek/Toltek.Bbb.Api/toltek.bbb.api.nginx /etc/bigbluebutton/nginx/
 service nginx reload
 
 ln -s /home/toltek/Toltek.Bbb.Api/toltek.bbb.api.service /etc/systemd/system/toltek.bbb.api.service
+sudo systemctl enable toltek.bbb.api.service
 sudo systemctl start toltek.bbb.api.service
 sudo systemctl status toltek.bbb.api.service
