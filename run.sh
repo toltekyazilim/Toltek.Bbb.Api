d38aec90a9e43c31da9d8d0cafda355bdbf63f6f
Toltek <yavuzcosar@gmail.com>
2021-10-05 15:27:43 +0300

    update

---
+++ run.sh
@@ -0,0 +1,20 @@
+﻿# Toltek Bigbluebutton Api - Update Bash
+# Yavuz 19/05/2020
+
+FILE=/etc/bigbluebutton/nginx/toltek.bbb.api.nginx
+if [ -f "$FILE" ]; then
+    systemctl stop toltek.bbb.api.service
+
+    rm /etc/systemd/system/toltek.bbb.api.service
+    ln -s /home/toltek/Toltek.Bbb.Api/toltek.bbb.api.service /etc/systemd/system/toltek.bbb.api.service
+
+    rm /etc/bigbluebutton/nginx/toltek.bbb.api.nginx
+    ln -s /home/toltek/Toltek.Bbb.Api/toltek.bbb.api.nginx /etc/bigbluebutton/nginx/toltek.bbb.api.nginx
+
+else
+    echo "$FILE does not exist."
+fi
+
+service nginx reload
+systemctl start toltek.bbb.api.service
+systemctl status toltek.bbb.api.service
